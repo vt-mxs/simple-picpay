352b746712a88e91dc6cde8638ffbc7b4f341ee3
vt-mxs <maximusvitor88@gmail.com>
2024-07-25 10:40:23 -0300

    feat: initial config

---
+++ src/main/java/com/simplepicpay/SimplePicpayApplication.java
@@ -0,0 +1,11 @@
+package com.simplepicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePicpayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SimplePicpayApplication.class, args);
+	}
+}
